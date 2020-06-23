6400c5a7debce023db17bfe7c8ad2cc3f8679010
haidousm <haidous.m@gmail.com>
2020-06-23 15:50:42 +0300

    create basic login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+    height: 250px;
+    width: 400px;
+
+    margin: 50px auto;
+
+    text-align: center;
+
+    border: 1px solid #000;
+    border-radius: 5px;
+`;
+
+const Heading = styled.h1`
+    font-color: black;
+`;
+
+const Button = styled.button`
+    color: white;
+    background-color: #000;
+
+    border: none;
+
+    padding: 30px;
+    border-radius: 5px;
+
+    font-size: 2rem;
+`;
+
 function Login() {
     return (
         <div>
-            <h1> Hello! </h1>
+            <LoginContainer>
+                <Heading>
+                    {" "}
+                    Welcome to <br></br> Moo
+                </Heading>
+                <Button>Login using Google</Button>
+            </LoginContainer>
         </div>
     );
 }

b3f4f761c252f4e554060412f20ac831f364923a
haidousm <haidous.m@gmail.com>
2020-06-23 15:42:12 +0300

    create react login & dashboard routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
     return (
-        <div>
-            <Dashboard />
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 

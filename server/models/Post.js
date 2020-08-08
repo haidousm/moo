4a8fb1145154bd117bd36af388af11c04acebf2f
haidousm <haidous.m@gmail.com>
2020-08-08 17:14:31 +0300

    edit Post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
     content: { type: String, required: true },
-    created: { type: Date, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
